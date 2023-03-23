b821d6a0e9e69d7b074acbbd513e4a826115d9ca
MedChouk <chouk.med20@gmail.com>
2023-03-23 23:43:17 +0100

    Syncing source code with build mounts

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:16.16.0-alpine
 WORKDIR /app
 COPY ./package.json ./
 RUN npm install 
-CMD ["node", "index.js"]
+# CMD ["node", "index.js"]
+CMD ["npm", "run", "dev"]
 EXPOSE 3000
 COPY . ./
--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express(); 
 
 app.get('/', function(req, res) { 
-    res.send("<h1> Hi There </h1>"); 
+    res.send("<h1> Hi There !!</h1>"); 
 }); 
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
